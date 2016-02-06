d6804659246ac8fde9beb6192a849f7c365034d2
Joshua L Geschwendt <joshua@geschwendt.com>
2016-02-05 19:15:15 -0500

    remove generated code from source control and add vscode settings

--- tmp/serve/client.js
+++
@@ -1,6 +0,0 @@
-var browser_1 = require('angular2/platform/browser');
-var router_1 = require('angular2/router');
-var app_1 = require('./app/app');
-browser_1.bootstrap(app_1.App, router_1.ROUTER_PROVIDERS.slice());
-
-//# sourceMappingURL=client.js.map
