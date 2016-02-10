fc6779bd503618457cde70e3f5f94d0060d64de4
asnow <asnow@st-10417s-mbp.springthrough.local>
2016-02-10 07:58:27 -0500

    Added scripts and modified .travis.yml for ci deploy from travis ci

---
+++ scripts/ci/deploy.sh
@@ -0,0 +1,29 @@
+
+#!/bin/bash
+set -ex
+
+MODE=$1
+
+echo =============================================================================
+# go to project dir
+SCRIPT_DIR=$(dirname $0)
+cd $SCRIPT_DIR/../..
+
+git config --global user.name "Travis-CI"
+git config --global user.email "travis@w3ctag.org"
+
+#if [[ $TRAVIS_BRANCH == 'master' ]]
+
+mkdir .publish
+cd .publish
+git init
+git clone https://github.com/SnowBiz/Angular2-Seed.git
+cd travis-deploy
+git checkout gh-pages
+rm -rf *
+mv ../../dist/* .
+git add -A
+git commit -m "Deploy from Travis CI"
+git push --set-upstream "https://${GH_TOKEN}@github.com/snowbiz/snowbiz.github.io.git" gh-pages
+
+#fi
